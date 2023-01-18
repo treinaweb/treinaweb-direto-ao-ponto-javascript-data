8da427f9c5f39c70b5a53526d268c9f7f3135e0a
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-01-18 16:36:42 -0300

    01:07 - Manipulando UTC

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-// const data = new Date('2023-04-16T12:10:05.123Z');
+const comUTC = new Date('2023-04-16T12:00:00.000Z');
+const semUTC = new Date('2023-04-16T12:00:00.000');
 
-// // altera o dia mês e ano
-// data.setFullYear(2022);
-// data.setMonth(0);
-// data.setDate(24);
+const data = new Date('2023-04-16T12:00:00.000-03:00');
 
-// altera a hora, minuto, segundo e milisegundos
-// data.setHours(20 - 3, 21, 10);
-// data.setMinutes(40);
-// data.setSeconds(15);
-// data.setMilliseconds(120)
-
-// console.log(data);
-
-
-const data = new Date('2023-04-16T12:10:05.123Z');
+console.log(comUTC);
+console.log(semUTC)
 console.log(data);
-setTimeout(() => {
-    const novoDia = data.getDate() - 5;
 
-    data.setDate(novoDia);
-    console.log(data);
-}, 2000)
+
+
