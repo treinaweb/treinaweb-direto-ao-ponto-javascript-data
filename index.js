f1d85b5ba9249caabe28bb4c440c3af78d2aba1d
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-01-18 16:37:29 -0300

    01:09 - Objeto Intl para formatar datas

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const data = new Date('2023-04-16T12:00:00.000Z');
 
-const dataFormatada = data.toLocaleDateString(['pt-BR', 'en-US'], {
-    // dateStyle: 'medium',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-    weekday: 'long',
-});
+const dataFormatada = new Intl.DateTimeFormat('pt-BR', {
+    dateStyle: 'short', timeStyle: 'short'
+}).format(data);
+
+
 
 console.log(dataFormatada);
