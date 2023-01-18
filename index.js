8f19717047588e3443d2ddee72b3ab054be3cd84
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-01-18 16:37:10 -0300

    01:08 - Método toLocaleDateString para formatar datas

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const comUTC = new Date('2023-04-16T12:00:00.000Z');
-const semUTC = new Date('2023-04-16T12:00:00.000');
+const data = new Date('2023-04-16T12:00:00.000Z');
 
-const data = new Date('2023-04-16T12:00:00.000-03:00');
+const dataFormatada = data.toLocaleDateString(['pt-BR', 'en-US'], {
+    // dateStyle: 'medium',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    weekday: 'long',
+});
 
-console.log(comUTC);
-console.log(semUTC)
-console.log(data);
-
-
-
+console.log(dataFormatada);
